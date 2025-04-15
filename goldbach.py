44806b3a78983362b2c82ccdc4067b2d562add4c
johannes <johannes.lewen@lewen.eu>
2025-04-15 14:40:35 +0200

    refactoring into functions

--- goldbach.py
+++ goldbach.py
@@ -1,28 +1,66 @@
-def isSumOfTwoPRIMES(number):
-  if number%2==1:
-    return False
-  for i in range(2, number):
-    good=True
-    # check if i is a prime
-    x = 2
-    while(x<i):
-      if(i%x==0):
-        good= False
-      x += 1
-    if good:
-      # i is a prime, now check if j=x-i is prime
-      j=number-i
-      if(j>=2):#j must be greater or equal 2 to be prime
-        good2 =True
-        x = 2
-        while(x<j):
-          if(j%x==0):
-            good2= False
-          x += 1
-        if good2:
-          print(f"The number {number} equals to the sum of {i} and {j}")
-  return True
+import re
+
+# start with the smallest prime number
+SMALLEST_PRIME = 2
+
+def is_sum_of_two_primes(input_number: int) -> bool:
+    """"""
+    catch_odd_numbers(input_number)
+    i = SMALLEST_PRIME
+    #  get the first prime number
+    while i <= input_number/2:
+        i_is_prime = check_is_prime(i)
+        if i_is_prime:
+            # now check if j (= input_number - i) is prime
+            j = input_number - i
+            if j >= SMALLEST_PRIME:
+                j_is_prime = check_is_prime(j)
+                if j_is_prime:
+                    print(f"The number {input_number} equals to the sum of {i} and {j}")
+        i += 1
+    return True
 
 
-Number=int(input("Enter a number:"))
-isSumOfTwoPRIMES(Number)
+def check_is_prime(i):
+    """If i is divisible by any divisor (excl. 1 and itself) it's not a prime.
+    Check this condition.
+    """
+    is_prime = True
+    divisor = SMALLEST_PRIME
+    while divisor < i:
+        if i % divisor == 0:
+            is_prime = False
+            break
+        divisor += 1
+    return is_prime
+
+
+def catch_odd_numbers(number):
+    # catch odd numbers
+    if number % 2 == 1:
+        get_user_number_input("Your number is an ODD number."
+                              "\nPlease enter an EVEN number:")
+
+
+def get_user_number_input(prompt):
+    """Get input that exclusively matches integer pattern
+    at the start of the string only accept one or more digits
+
+    :param prompt:
+    """
+    pattern = re.compile(r"^\d+$")
+    input_number = input(prompt).strip()
+    if pattern.match(input_number):
+        input_number = int(input_number)
+    else:
+        print(f"You entered '{input_number}'. That's not a positive integer.")
+        get_user_number_input(prompt)
+    return input_number
+
+
+def main():
+    input_number = get_user_number_input("Enter an integer:")
+    is_sum_of_two_primes(input_number)
+
+if __name__ == '__main__':
+    main()
