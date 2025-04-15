f24abf16864de0ecbbf7030f79d4fa8eb2296440
johannes <johannes.lewen@lewen.eu>
2025-04-15 15:26:51 +0200

    finalize refactoring

--- goldbach.py
+++ goldbach.py
@@ -3,64 +3,77 @@ import re
 # start with the smallest prime number
 SMALLEST_PRIME = 2
 
-def is_sum_of_two_primes(input_number: int) -> bool:
-    """"""
-    catch_odd_numbers(input_number)
+
+def generate_prime_tuples(input_number: int) -> list[tuple[int, int]]:
+    """Assuming Goldbach’s conjecture that every even natural number greater than 2
+    is the sum of two prime numbers, generate the tuple(s) of primes that make up the input number.
+
+    :param input_number: integer to be split into two primes.
+    :return: A list of tuples containing two prime numbers as integers each.
+    """
+    input_number = handle_odd_number_input(input_number)
+    list_of_primes = []
     i = SMALLEST_PRIME
     #  get the first prime number
-    while i <= input_number/2:
-        i_is_prime = check_is_prime(i)
-        if i_is_prime:
+    while i <= input_number // 2:
+        if check_is_prime(i):
             # now check if j (= input_number - i) is prime
             j = input_number - i
-            if j >= SMALLEST_PRIME:
-                j_is_prime = check_is_prime(j)
-                if j_is_prime:
-                    print(f"The number {input_number} equals to the sum of {i} and {j}")
+            if check_is_prime(j):
+                list_of_primes.append((i, j))
         i += 1
+    return list_of_primes
+
+
+def check_is_prime(n: int) -> bool:
+    """If i is divisible by any divisor (excl. 1 and itself) it's not a prime.
+    Return True only if n is a prime number.
+
+    :param n: integer to be check whether it is a prime number
+    """
+    if n <= 2:
+        return False
+    for divisor in range(2, n):
+        if n % divisor == 0:
+            return False
     return True
 
 
-def check_is_prime(i):
-    """If i is divisible by any divisor (excl. 1 and itself) it's not a prime.
-    Check this condition.
+def handle_odd_number_input(number: int) -> int:
+    """Catch odd numbers from the input,
+    since they are not covered by the Goldbach conjecture.
+    Handles recursive calling for input in case of odd numbers.
+
+    :param number: integer to be checked whether even or odd
+    :return: an even integer
     """
-    is_prime = True
-    divisor = SMALLEST_PRIME
-    while divisor < i:
-        if i % divisor == 0:
-            is_prime = False
-            break
-        divisor += 1
-    return is_prime
-
-
-def catch_odd_numbers(number):
-    # catch odd numbers
     if number % 2 == 1:
-        get_user_number_input("Your number is an ODD number."
-                              "\nPlease enter an EVEN number:")
+        number = get_user_number_input("Your number is an ODD number."
+                                       "\nPlease enter an EVEN number:")
+    return number
 
 
-def get_user_number_input(prompt):
+def get_user_number_input(prompt: str) -> int:
     """Get input that exclusively matches integer pattern
     at the start of the string only accept one or more digits
 
-    :param prompt:
+    :param prompt: String to prompt user input.
+    :return: The user input converted from str to int
     """
     pattern = re.compile(r"^\d+$")
-    input_number = input(prompt).strip()
-    if pattern.match(input_number):
-        input_number = int(input_number)
-    else:
-        print(f"You entered '{input_number}'. That's not a positive integer.")
-        input_number = get_user_number_input(prompt)
-    return input_number
+    while True:
+        user_input = input(prompt).strip()
+        if pattern.match(user_input):
+            return int(user_input)
+        print(f"You entered '{user_input}'. That's not a positive integer.")
 
 
 def main():
     input_number = get_user_number_input("Enter an integer:")
-    is_sum_of_two_primes(input_number)
+    list_of_prime_tuples = generate_prime_tuples(input_number)
+    for i, j in list_of_prime_tuples:
+        print(f"The number {input_number} equals to the sum of {i} and {j}")
+
 
 if __name__ == '__main__':
     main()
